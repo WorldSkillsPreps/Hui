b16f5b38db44fa3f0d80838ed6539205c9ed3e66
LAV-cloud <ZombieManiak172@outlook.com>
2020-12-31 20:31:23 +0700

    Initial Commit

---
+++ Quantum wallet/Quantum_walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quantum_walletApp.swift
+//  Quantum wallet
+//
+//  Created by Ромка Бережной on 31.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct Quantum_walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
